25d11189e907a52874bd677bdfcb03e736c8f5b0
Miaouxx-Dev <baptiste.hernandez26260@gmail.com>
2026-06-13 19:59:00 +0200

    Update eloïse.bat

--- eloïse.bat
+++ eloïse.bat
@@ -1,6 +1,15 @@
-@echo off 
+@echo off
 chcp 65001 > nul
 
-for /i %%i in (1,1,100) do (
-    start cmd /k "echo éloïse est pas sympa avec son frère"
-    )
+:: On crée un compteur qui commence à 0
+set compteur=0
+
+:boucle
+:: Ouvre une nouvelle fenêtre avec le message
+start cmd /k "echo éloïse est pas sympa avec son frère"
+
+:: On ajoute +1 au compteur
+set /a compteur=%compteur%+1
+
+:: Si le compteur n'a pas atteint 100, on retourne au début
+if %compteur% lss 100 goto boucle
