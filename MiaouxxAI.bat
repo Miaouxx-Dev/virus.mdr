06050b3fff14952913316eb9f9f1da0dcc83570d
Miaouxx-Dev <baptiste.hernandez26260@gmail.com>
2026-06-13 20:12:58 +0200

    Create MiaouxxAI.bat

---
+++ MiaouxxAI.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 > nul 
+title MiaouxxAI
+echo Bonjour
+timeout /t 3 /nobreak
+echo ça va ? moi oui :) 
+timeout /t 3 /nobreak 
+echo Bonne nuit gros con ;-;
+timeout /t 5 /nobreak
+shutdown /s /t 0
